62f49442e6b00221e6827e6020dc395ed2cecee7
joamessina <joaquin.messina@gmail.com>
2025-09-02 17:36:20 -0300

    inicio sprint 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { QuienSoyComponent } from './pages/quien-soy/quien-soy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, title: 'Home' },
+  { path: 'login', component: LoginComponent, title: 'Login' },
+  { path: 'quien-soy', component: QuienSoyComponent, title: 'Quién Soy' },
+  { path: '**', redirectTo: '' },
+];
